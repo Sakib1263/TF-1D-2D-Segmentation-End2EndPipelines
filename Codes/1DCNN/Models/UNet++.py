bdeeed8913686d5141a5178bddc0137cce3f7212
Sakib Mahmud <sakib.mahmud@qu.edu.qa>
2022-02-14 02:14:26 +0300

    Update UNet++.py

--- Codes/1DCNN/Models/UNet++.py
+++ Codes/1DCNN/Models/UNet++.py
@@ -129,6 +129,7 @@ class UNet:
             pool = Feature_Extraction_Block(pool, self.model_width, self.feature_number)
         conv = Conv_Block(pool, self.model_width, self.kernel_size, 2 ** self.model_depth)
         conv = Conv_Block(conv, self.model_width, self.kernel_size, 2 ** self.model_depth)
+        convs["conv%s" % (self.model_depth + 1)] = conv
 
         # Decoding
         convs_list = list(convs.values())
